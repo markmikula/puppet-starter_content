9ce17f0e240363729732781dca54dcfb6c3f0087
markmikula <43763946+markmikula@users.noreply.github.com>
2018-10-09 13:41:06 -0400

    changes

--- site/profile/manifests/windows_baseline.pp
+++ site/profile/manifests/windows_baseline.pp
@@ -1,16 +1,10 @@
 # @summary This profile installs unzip and git as part of the Windows baseline
 class profile::windows_baseline {
 
-  include chocolatey
-
-  package { 'unzip':
-    ensure   => installed,
-    provider => chocolatey,
-  }
-
-  package { 'git':
-    ensure   => installed,
-    provider => chocolatey,
-  }
+exec { 'winrm-enable':
+  provider => powershell,
+  command => "winrm quickconfig -silent"
+  unless => "if (test-wsman) { exit 1 }"
+}
 
 }
